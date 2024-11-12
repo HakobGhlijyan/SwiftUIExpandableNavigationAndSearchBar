4b8d6bdf844e425a1036549198eb3effcda5f626
Ghlijyan Hakob <hakobghlijyan@gmail.com>
2024-11-12 16:26:57 +0400

    Initial Commit

---
+++ SwiftUIExpandableNavigationAndSearch Bar/SwiftUIExpandableNavigationAndSearch_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExpandableNavigationAndSearch_BarApp.swift
+//  SwiftUIExpandableNavigationAndSearch Bar
+//
+//  Created by Hakob Ghlijyan on 12.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExpandableNavigationAndSearch_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
